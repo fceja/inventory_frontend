e8460cd80791c6c52729cabbf6c2a98d9d03c56a
fceja <88167774+fceja@users.noreply.github.com>
2024-03-12 09:03:03 -0700

    add browser router and redux

--- src/ts/components/Root.tsx
+++ src/ts/components/Root.tsx
@@ -1,7 +1,18 @@
 import App from "./App";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Provider } from "react-redux";
+import Store from "../store/ConfigureStore";
 
 const Root = () => {
-  return <App />;
+  return (
+    <Provider store={Store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </Provider>
+  );
 };
 
 export default Root;
