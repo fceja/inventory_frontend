406643238fb0defdccd294c8cbd41b0339e760e0
fceja <88167774+fceja@users.noreply.github.com>
2024-03-12 17:50:53 -0700

    add update component and scss

--- src/ts/components/App.tsx
+++ src/ts/components/App.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import HomePage from "../pages/HomePage";
 import UpdatePage from "../pages/UpdatePage";
+import "../../scss/App.scss";
 
 const App = () => {
   return (
--- src/ts/pages/UpdatePage.tsx
+++ src/ts/pages/UpdatePage.tsx
@@ -1,5 +1,37 @@
+import "../../scss/UpdatePage.scss";
+
 const UpdatePage = () => {
-  return <div>This is update page.</div>;
+  return (
+    <>
+      <div className="upload-image shadow">Image container.</div>
+      <div className="main-container shadow">
+        <div className="item-title">Item folder</div>
+        <div className="item-name">Enter Item Name</div>
+        <div className="item-details">
+          <div className="items-top">
+            <div className="quantity-container">
+              <label className="quantity-label">quantity</label>
+              <input type="number" min="0" id="quantity" />
+            </div>
+            <div className="min-level-container">
+              <label className="min-level">min level</label>
+              <input type="number" min="0" id="min-level" />
+            </div>
+          </div>
+          <div className="items-bot">
+            <div className="price-container">
+              <label className="price">price</label>
+              <input type="number" min="0" id="price" />
+            </div>
+            <div className="total-container">
+              <label className="total">total</label>
+              <input type="number" min="0" id="total" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default UpdatePage;
