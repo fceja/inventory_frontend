f5468afdffa19f1929be3cce56b2fd975e61e1e9
fceja <88167774+fceja@users.noreply.github.com>
2024-03-19 09:41:11 -0700

    include placeholder reducer

--- src/ts/store/RootReducer.ts
+++ src/ts/store/RootReducer.ts
@@ -1,5 +1,15 @@
+import { Action } from "redux";
+
 import { combineReducers } from "@reduxjs/toolkit";
 
-const rootReducer = combineReducers({});
+const placeholderReducer = (state = {}, _action: Action) => {
+  // reducer does nothing, it returns the initial state
+  // including to statisfy console error
+  return state;
+};
+
+const rootReducer = combineReducers({
+  placeholder: placeholderReducer,
+});
 
 export default rootReducer;
