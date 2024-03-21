0b93654c1eec8536f3e6a6bbe7845308a0f628ef
fceja <88167774+fceja@users.noreply.github.com>
2024-03-21 10:30:35 -0700

    update axios config to parse and apply token to subsequent requests

--- src/api/config/AxiosConfig.ts
+++ src/api/config/AxiosConfig.ts
@@ -5,5 +5,22 @@ export const apiClient = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
 });
 
-// leaving empty, since we don't want axios errors logged
-apiClient.interceptors.response.use(undefined, () => {});
+apiClient.interceptors.response.use(
+  /**
+   * parse authorization bearer token from response and
+   * apply to subsequent axios client requests
+   */
+  (response) => {
+    const token = response.headers["authorization"];
+    if (token) {
+      apiClient.defaults.headers.common["authorization"] = `${token}`;
+    }
+
+    return response;
+  },
+
+  // leaving empty, since we don't want axios errors logged
+  (_error) => {
+    return;
+  },
+);
