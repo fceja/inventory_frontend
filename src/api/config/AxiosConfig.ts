1b417f4ad2abe7ee42e548c7f7488df5dd9e3388
fceja <88167774+fceja@users.noreply.github.com>
2024-03-26 15:37:16 -0700

    add request inteceptor to axios config

--- src/api/config/AxiosConfig.ts
+++ src/api/config/AxiosConfig.ts
@@ -3,13 +3,27 @@ import { Dispatch } from "redux";
 
 import { loginUser, AuthActionT } from "../../ts/store/auth/authActions";
 
-const useApiClient = (dispatch: Dispatch<AuthActionT>) => {
+const useApiClient = (dispatch: Dispatch<AuthActionT>, authState?: any) => {
   const apiClient = axios.create({
     withCredentials: true,
     baseURL: import.meta.env.VITE_API_BASE_URL,
   });
 
-  // Add interceptor for responses
+  // Add interceptor for request
+  apiClient.interceptors.request.use(
+    (request) => {
+      if (authState) {
+        request.headers["authorization"] = authState.authToken;
+      }
+
+      return request;
+    },
+    (error) => {
+      return Promise.reject(error);
+    },
+  );
+
+  // Add interceptor for response
   apiClient.interceptors.response.use(
     (response) => {
       const token: string = response.headers["authorization"];
