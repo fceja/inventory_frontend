90f6a6316ca71cd10df083b48381a4febb8a642c
fceja <88167774+fceja@users.noreply.github.com>
2024-03-27 09:57:44 -0700

    add vite alias path config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@api": path.resolve(__dirname, "./src/api"),
+      "@components": path.resolve(__dirname, "./src/ts/components"),
+      "@pages": path.resolve(__dirname, "./src/ts/pages"),
+      "@scss": path.resolve(__dirname, "./src/scss"),
+      "@store": path.resolve(__dirname, "./src/ts/store"),
+    },
+  },
+});
