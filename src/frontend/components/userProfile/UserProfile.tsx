c33bed3ea2f547ae219e650681bc9872c7d0ce81
fceja <88167774+fceja@users.noreply.github.com>
2024-04-01 21:20:14 -0700

    fix user img container css

---
+++ src/frontend/components/userProfile/UserProfile.tsx
@@ -0,0 +1,9 @@
+const UserProfile = () => {
+  return (
+    <div className="user-profile">
+      <div className="img-container">Image container</div>
+    </div>
+  );
+};
+
+export default UserProfile;
