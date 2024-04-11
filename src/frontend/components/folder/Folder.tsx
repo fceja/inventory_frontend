b325c69e3a3997b0dac90a011362b2de201b0fa3
fceja <88167774+fceja@users.noreply.github.com>
2024-04-11 08:25:54 -0700

    wip, folderNodes component

--- src/frontend/components/folder/Folder.tsx
+++ src/frontend/components/folder/Folder.tsx
@@ -1,7 +1,42 @@
-const Folder = () => {
-    return (
-        <div className="folder">This is folder.</div>
-    )
+import { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { RootState } from "@store/ConfigureStore";
+import FolderNodesApi from "@api/FolderNodesApi"
+
+interface FolderNodesI {
+    name: string,
+    nodeType: string;
 }
 
-export default Folder
+const Folder = () => {
+    const dispatch = useDispatch();
+    const authState = useSelector((state: RootState) => state.authState);
+    const [data, setData] = useState<FolderNodesI[] | null>(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await FolderNodesApi(dispatch, authState).get(0); // zero for root
+            if (response && response.status === 200 && response.data.success) {
+                console.log(response.data.folderNodes);
+                setData(response.data.folderNodes);
+            }
+        };
+        fetchData();
+    }, []);
+
+    return (
+        <>
+            <div className="folder">This is folder.</div>
+            {data && (
+                <>
+                    {data.map((node, index) => (
+                        <div key={index} className={`${node.nodeType}-node`}>{`${node.name} ${node.nodeType}`}</div>
+                    ))}
+                </>
+            )}
+        </>
+    );
+};
+
+export default Folder;
