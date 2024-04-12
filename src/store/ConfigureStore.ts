dc4c90988bc66003ebe87d9e76def2c45c608fe3
fceja <88167774+fceja@users.noreply.github.com>
2024-04-11 18:08:48 -0700

    save and load redux store

--- src/store/ConfigureStore.ts
+++ src/store/ConfigureStore.ts
@@ -3,11 +3,20 @@ import { configureStore } from "@reduxjs/toolkit";
 import { checkTokenExpiryMiddleware } from "@store/middleware/authMidW";
 import rootReducer from "@store/RootReducer";
 
+const savedState = localStorage.getItem('reduxState');
+const preloadedState = savedState ? JSON.parse(savedState) : {}
+
 const store = configureStore({
   reducer: rootReducer,
+  preloadedState,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(checkTokenExpiryMiddleware),
 });
+
+
+store.subscribe(() => {
+  localStorage.setItem('reduxState', JSON.stringify(store.getState()))
+})
 
 export type RootState = ReturnType<typeof store.getState>;
 
