1a989717183d6893cc58b8b93905d93b455bfb57
fceja <88167774+fceja@users.noreply.github.com>
2024-04-12 16:35:04 -0700

    add closing x link

--- src/frontend/pages/ItemPage.tsx
+++ src/frontend/pages/ItemPage.tsx
@@ -3,15 +3,19 @@ import { useParams, Link } from "react-router-dom";
 import { useDispatch, useSelector } from 'react-redux';
 import { Dispatch } from 'redux';
 
+import "@scss/pages/ItemPage.scss"
 import { AuthActionT } from "@store/auth/authActions";
 import { RootState } from "@store/ConfigureStore";
 import ItemsApi from "@api/ItemsApi"
 
 interface ItemDataI {
-    parentFolderId: number
+    parentFolderId: number,
+    name: string
 }
 
 const ItemPage = () => {
+    const [itemName, setItemName] = useState<string>('Item not found')
+    const [folderPath, setFolderPath] = useState<string>('/folders/main')
     const [itemData, setItemData] = useState<ItemDataI | null>(null)
     const dispatch: Dispatch<AuthActionT> = useDispatch();
     const authState = useSelector((state: RootState) => state.authState);
@@ -28,13 +32,32 @@ const ItemPage = () => {
 
     }, [])
 
+    useEffect(() => {
+        console.log('in here itemdata')
+
+        if (itemData) {
+            setItemName(itemData.name)
+            setFolderPath(`/folders/${itemData.parentFolderId}`)
+
+        }
+
+    }, [itemData])
+
     return (
-        <>
-            <div>This is ItemPage</div>
-            {itemData &&
-                <Link to={`/folders/${itemData.parentFolderId}`}>This is link. TODO.</Link>
-            }
-        </>
+        <div className="item-header">
+            <>
+                <div>{`Item name: ${itemName}`}</div>
+                <Link to={`${folderPath}`}>
+                    <div
+                        className="item-ham-menu-bar-container"
+                    >
+                        <div className="item-ham-bar clicked"></div>
+                        <div className="item-ham-bar clicked"></div>
+                        <div className="item-ham-bar clicked"></div>
+                    </div>
+                </Link>
+            </>
+        </div>
     )
 }
 
