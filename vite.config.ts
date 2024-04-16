0d854b63e54f4d62413779b0bfdbfed883417331
fceja <88167774+fceja@users.noreply.github.com>
2024-04-16 16:58:27 -0700

    add common alias path

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig({
     alias: {
       "@api": path.resolve(__dirname, "./src/api"),
       "@components": path.resolve(__dirname, "./src/frontend/components"),
+      "@common": path.resolve(__dirname, "./src/frontend/common"),
       "@hooks": path.resolve(__dirname, "./src/frontend/hooks"),
       "@pages": path.resolve(__dirname, "./src/frontend/pages"),
       "@scss": path.resolve(__dirname, "./src/scss"),
