6ba2920d2783502603427a05d44156415b598fde
fceja <88167774+fceja@users.noreply.github.com>
2024-04-16 13:38:33 -0700

    refactor folder actions and reducer

--- src/store/folder/folderActions.ts
+++ src/store/folder/folderActions.ts
@@ -1,17 +1,28 @@
-import { Action } from "redux";
+export const SET_FOLDER_LEVEL = "SET_FOLDER_LEVEL"
+export const SET_CUR_LEVEL_FOLDER_ID = "SET_CUR_LEVEL_FOLDER_ID"
 
-const SET_FOLDER_LEVEL = "SET_FOLDER_LEVEL"
-
-interface SetFolderLevelAction extends Action {
+interface SetFolderLevelAction {
     type: typeof SET_FOLDER_LEVEL;
     payload: { folderLevel: string };
 }
 
-export type FolderActionT = SetFolderLevelAction
+interface SetCurLevelFolderIdAction {
+    type: typeof SET_CUR_LEVEL_FOLDER_ID;
+    payload: { curLevelFolderId: string };
+}
+
+export type FolderActionT = SetFolderLevelAction | SetCurLevelFolderIdAction
 
 export const setFolderLevel = (level: string): FolderActionT => {
     return {
         type: SET_FOLDER_LEVEL,
         payload: { folderLevel: level }
+    }
+}
+
+export const setCurLevelFolderId = (folderId: string): FolderActionT => {
+    return {
+        type: SET_CUR_LEVEL_FOLDER_ID,
+        payload: { curLevelFolderId: folderId }
     }
 }
--- src/store/folder/folderReducer.ts
+++ src/store/folder/folderReducer.ts
@@ -1,19 +1,20 @@
-import { FolderActionT } from "@store/folder/folderActions";
+import {
+    SET_FOLDER_LEVEL,
+    SET_CUR_LEVEL_FOLDER_ID,
+    FolderActionT
+} from "@store/folder/folderActions";
 
-interface InitialStateI {
-    folderLevel: string | null;
-}
-
-const initialState: InitialStateI = {
-    folderLevel: null
-}
-
-const folderReducer = (state = initialState, action: FolderActionT) => {
+const folderReducer = (state = {}, action: FolderActionT) => {
     switch (action.type) {
-        case "SET_FOLDER_LEVEL":
+        case SET_FOLDER_LEVEL:
             return {
                 ...state,
                 folderLevel: action.payload.folderLevel
+            }
+        case SET_CUR_LEVEL_FOLDER_ID:
+            return {
+                ...state,
+                curLevelFolderId: action.payload.curLevelFolderId
             }
 
         default:
