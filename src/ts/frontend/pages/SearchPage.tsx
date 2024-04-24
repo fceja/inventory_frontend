cf488a4da5923ea1f4eed2f9a2b629af87e97658
fceja <88167774+fceja@users.noreply.github.com>
2024-04-24 08:49:04 -0700

    adjust error styling on search page

--- src/ts/frontend/pages/SearchPage.tsx
+++ src/ts/frontend/pages/SearchPage.tsx
@@ -9,12 +9,14 @@ import SearchApi from "@api/SearchApi";
 
 const SearchPage = () => {
     const [searchInput, setSearchInput] = useState("");
+    const [isSearchInputDisabled, setIsSearchInputDisabled] = useState(false)
     const [lastSearchTerm, setlastSearchTerm] = useState("");
     const [foldersLastSearchTerm, setFoldersLastSearchTerm] = useState("");
     const [itemsLastSearchTerm, setItemsLastSearchTerm] = useState("");
 
     const [includeFolders, setIncludeFolders] = useState(true);
     const [includeItems, setIncludeItems] = useState(true);
+    const [showCheckboxError, setShowCheckboxError] = useState(false)
 
     const [foldersData, setFoldersData] = useState(null);
     const [itemsData, setItemsData] = useState(null);
@@ -75,7 +77,6 @@ const SearchPage = () => {
             setFoldersLastSearchTerm(searchInput)
             fetchData();
         }
-
     }, [includeFolders])
 
     useEffect(() => {
@@ -84,44 +85,59 @@ const SearchPage = () => {
             setItemsLastSearchTerm(searchInput)
             fetchData();
         }
-
     }, [includeItems])
+
+    useEffect(() => {
+        if (!includeFolders && !includeItems) {
+            setIsSearchInputDisabled(true)
+            setShowCheckboxError(true)
+        } else {
+            setIsSearchInputDisabled(false)
+            setShowCheckboxError(false)
+        }
+    }, [includeFolders, includeItems])
 
 
     return (
         <>
             <div className="search-content">
                 <form onSubmit={(event) => event.preventDefault()}>
-
                     <input
                         type="search"
                         value={searchInput}
+                        id={"search-input"}
                         placeholder="Search..."
                         onChange={(event) => handleInputChange(event)}
                         onKeyDown={(event) => handleKeyPress(event)}
+                        disabled={isSearchInputDisabled}
                     />
-                    <div className="folders-checkbox">
-                        <label htmlFor="folders">Folders</label>
-                        <input
-                            type="checkbox"
-                            id="foldersCheckbox"
-                            name="folders"
-                            onChange={(event) => setIncludeFolders(event.target.checked)}
-                            // onChange={handleFoldersCheckBox}
-                            checked={includeFolders}
-                        />
-                    </div>
-                    <div className="items-checkbox">
-                        <label htmlFor="items">Items</label>
-                        <input
-                            type="checkbox"
-                            id="itemsCheckbox"
-                            name="items"
-                            onChange={(event) => setIncludeItems(event.target.checked)}
-                            checked={includeItems}
-                        />
+                    <div className="checkboxes">
+                        <label htmlFor={"folders"} className={`${showCheckboxError && "error-underline"}`}>
+                            Folders
+                            <input
+                                type="checkbox"
+                                id="foldersCheckbox"
+                                name="folders"
+                                onChange={(event) => setIncludeFolders(event.target.checked)}
+                                checked={includeFolders}
+                            />
+
+                        </label>
+                        <label htmlFor="items" className={`${showCheckboxError && "error-underline"}`}>
+                            Items
+                            <input
+                                type="checkbox"
+                                id="itemsCheckbox"
+                                name="items"
+                                onChange={(event) => setIncludeItems(event.target.checked)}
+                                checked={includeItems}
+                            />
+                        </label>
                     </div>
                 </form>
+                {showCheckboxError &&
+                    <div className={"folder-item-error error"}>Must select at least one option.</div>
+                }
             </div>
             <div className="search-results">
                 {includeFolders && foldersData && handleSubmit &&
