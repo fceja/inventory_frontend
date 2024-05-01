c4e985cb282d67b4cfa3f6736922162ec6135814
fceja <88167774+fceja@users.noreply.github.com>
2024-05-01 15:52:47 -0700

    add simple css to not found page

--- src/ts/frontend/pages/NotFoundPage.tsx
+++ src/ts/frontend/pages/NotFoundPage.tsx
@@ -1,5 +1,16 @@
 const NotFoundPage = () => {
-    return (<div>Not Found Page.</div>)
+    return (
+        <div
+            className="not-found-page"
+            style={{
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+                height: '200px'
+            }}
+        >
+            Not Found Page.</div >
+    )
 }
 
 export default NotFoundPage
