d4c7ee4db1909cf059426acce2402d58c4baa232
fceja <88167774+fceja@users.noreply.github.com>
2024-04-30 17:25:06 -0700

    add missing header for modal

--- src/ts/frontend/components/modals/AddItemModal.tsx
+++ src/ts/frontend/components/modals/AddItemModal.tsx
@@ -1,15 +1,32 @@
+import { useDispatch } from 'react-redux';
+import { Dispatch } from 'redux';
+
 import "@scss/components/modals/AddItemModal.scss"
 import ItemForm from "@components/forms/ItemForm"
 import Modal from "@components/modals/Modal";
-import { setIsAddItemModalOpen } from "@store/modal/ModalActions";
+import { setIsAddItemModalOpen, ModalActionT } from "@store/modal/ModalActions";
 
+/* Note: some classNames' use scss styling from Modal.scss */
 const AddItemModal = () => {
+    const dispatch: Dispatch<ModalActionT> = useDispatch();
+
+    const handleCloseClick = () => {
+        dispatch(setIsAddItemModalOpen(false))
+    };
+
     return (
         <Modal
             className="add-item-modal"
-            dispatchCallBack={() => setIsAddItemModalOpen(false)}
-            hasHeader={true}
         >
+            <div className="modal-header">
+                <div
+                    className="modal-close"
+                    onClick={() => handleCloseClick()}
+                >
+                    <div className="modal-close-bar"></div>
+                    <div className="modal-close-bar"></div>
+                </div>
+            </div>
             <div className="add-item-upload-image shadow">Item image container.</div>
             <ItemForm />
         </Modal>
