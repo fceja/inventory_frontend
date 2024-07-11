f55d6512f30c72a62ed6dcea691bb16773007631
fceja <88167774+fceja@users.noreply.github.com>
2024-07-10 19:55:27 -0700

    remove cancel implementation since axios can handle

--- src/ts/api/FoldersApi.ts
+++ src/ts/api/FoldersApi.ts
@@ -1,35 +1,23 @@
 import useApiClient from "@api/config/AxiosConfig";
-import { defineCancelApiObject } from "@api/utils/AxiosUtils";
 
 const FoldersApi = () => {
     const apiClient = useApiClient();
-    const cancelApiObject = defineCancelApiObject(FoldersApi)
 
-    const getAggregatedDataByFolderId = async (folderId: number, cancel = false) => {
-        const cancelSignal =
-            cancel && cancelApiObject
-                ? cancelApiObject.get.handleRequestCancellation().signal
-                : undefined;
+    const getAggregatedDataByFolderId = async (folderId: number) => {
 
         return await apiClient.request({
             url: `/folders/${folderId}/aggregatedData`,
             method: "GET",
             data: null,
-            signal: cancelSignal
         })
     };
 
-    const getByFolderId = async (folderId: number, cancel = false) => {
-        const cancelSignal =
-            cancel && cancelApiObject
-                ? cancelApiObject.get.handleRequestCancellation().signal
-                : undefined;
+    const getByFolderId = async (folderId: number) => {
 
         return await apiClient.request({
             url: `/folders/${folderId}`,
             method: "GET",
             data: null,
-            signal: cancelSignal
         })
     };
 
--- src/ts/api/ProductsApi.ts
+++ src/ts/api/ProductsApi.ts
@@ -1,21 +1,14 @@
 import useApiClient from "@api/config/AxiosConfig";
-import { defineCancelApiObject } from "@api/utils/AxiosUtils";
 
 const ProductsApi = () => {
   const apiClient = useApiClient();
-  const cancelApiObject = defineCancelApiObject(ProductsApi);
 
-  const get = async (cancel = false) => {
-    const cancelSignal =
-      cancel && cancelApiObject
-        ? cancelApiObject.get.handleRequestCancellation().signal
-        : undefined;
+  const get = async () => {
 
     return await apiClient.request({
       url: "/products",
       method: "GET",
       data: null,
-      signal: cancelSignal,
     });
   };
 
