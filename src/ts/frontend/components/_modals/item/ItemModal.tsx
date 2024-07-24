5e517375fc8bba426e69e0f71f7737f6d75cc6dc
fceja <88167774+fceja@users.noreply.github.com>
2024-07-24 10:12:03 -0700

    add readonly mode to search page item node

--- src/ts/frontend/components/_modals/item/ItemModal.tsx
+++ src/ts/frontend/components/_modals/item/ItemModal.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useRef, useState } from "react"
 import { Dispatch } from 'redux';
 import { useDispatch, useSelector } from 'react-redux';
 
@@ -13,6 +13,8 @@ import { setIsItemModalOpen, ModalActionT } from "@store/modal/ModalActions";
 const ItemModal = () => {
   const dispatch: Dispatch<ItemActionT | ModalActionT> = useDispatch();
   const { selectedItemId } = useSelector((state: RootState) => state.itemState);
+  const { userRole } = useSelector((state: RootState) => state.userState);
+  const isEditorRef = useRef(false)
 
   const [isConfirmDeleteOpen, setIsConfirmDeleteOpen] = useState(false)
   const [isOptionsMenuOpen, setIsOptionsMenuOpen] = useState(false)
@@ -36,9 +38,15 @@ const ItemModal = () => {
     setIsConfirmDeleteOpen(false)
   }
 
-  const handleHeaderOptionsClick = () => {
-    isOptionsMenuOpen ? setIsOptionsMenuOpen(false) : setIsOptionsMenuOpen(true)
+  const handleHeaderOptionsClick = () => { setIsOptionsMenuOpen(!isOptionsMenuOpen) }
+
+  if (!userRole) {
+    isEditorRef.current = false
   }
+  else if (["editor", "publisher", "admin"].includes(userRole)) {
+    isEditorRef.current = true
+  }
+  else { isEditorRef.current = false }
 
   return (
     <Modal className="item-modal">
@@ -54,17 +62,40 @@ const ItemModal = () => {
               viewBox="0 0 512 512">
               <path d="M471.6 21.7c-21.9-21.9-57.3-21.9-79.2 0L362.3 51.7l97.9 97.9 30.1-30.1c21.9-21.9 21.9-57.3 0-79.2L471.6 21.7zm-299.2 220c-6.1 6.1-10.8 13.6-13.5 21.9l-29.6 88.8c-2.9 8.6-.6 18.1 5.8 24.6s15.9 8.7 24.6 5.8l88.8-29.6c8.2-2.7 15.7-7.4 21.9-13.5L437.7 172.3 339.7 74.3 172.4 241.7zM96 64C43 64 0 107 0 160V416c0 53 43 96 96 96H352c53 0 96-43 96-96V320c0-17.7-14.3-32-32-32s-32 14.3-32 32v96c0 17.7-14.3 32-32 32H96c-17.7 0-32-14.3-32-32V160c0-17.7 14.3-32 32-32h96c17.7 0 32-14.3 32-32s-14.3-32-32-32H96z" />
             </svg>
-            {isOptionsMenuOpen &&
+            {/* read only mode */}
+            {!isEditorRef.current && isOptionsMenuOpen &&
+              <span className="disabled-text">Feature disabled. <br /> READ-ONLY mode.
+                <button onClick={() => { setIsOptionsMenuOpen(false) }}>Close</button>
+              </span>
+            }
+            {/* editor mode */}
+            {isEditorRef.current && isOptionsMenuOpen &&
               <div className="header-item-menu-options">
                 <ul>
-                  <li onClick={() => { console.log('TODO - Edit') }}>Edit</li>
-                  <li onClick={() => { console.log('TODO - Move') }}>Move</li>
-                  <li onClick={() => { setIsConfirmDeleteOpen(true) }}>Delete</li>
-                  <li onClick={() => { setIsOptionsMenuOpen(false) }}>Close</li>
+                  <li>
+                    <button
+                      onClick={() => { console.log('TODO - Edit') }}>Edit
+                    </button>
+                  </li>
+                  <li>
+                    <button
+                      onClick={() => { console.log('TODO - Move') }}>Move
+                    </button>
+                  </li>
+                  <li>
+                    <button
+                      onClick={() => { setIsConfirmDeleteOpen(true) }}>Delete
+                    </button>
+                  </li>
+                  <li>
+                    <button
+                      onClick={() => { setIsOptionsMenuOpen(false) }}>Close
+                    </button>
+                  </li>
                 </ul>
               </div>
             }
-          </div>
+          </div >
           <div
             className="item-modal-close"
             onClick={() => handleCloseClick()}
@@ -72,10 +103,11 @@ const ItemModal = () => {
             <div className="item-modal-close-bar"></div>
             <div className="item-modal-close-bar"></div>
           </div>
-        </div>
+        </div >
       </>
       <ItemNodeModal />
-      {isConfirmDeleteOpen &&
+      {
+        isConfirmDeleteOpen &&
         <div className="item-modal-confirm-delete">
           Are you sure you want to delete this item? <br />
           <div className="confirm-btns">
@@ -84,7 +116,7 @@ const ItemModal = () => {
           </div>
         </div>
       }
-    </Modal>
+    </Modal >
   );
 };
 
