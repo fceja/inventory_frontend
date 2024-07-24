6a75f2daf448df698a8c52d2219c782b7ae252b0
fceja <88167774+fceja@users.noreply.github.com>
2024-07-24 09:37:16 -0700

    add read only user mode for adding node

--- src/ts/frontend/components/mainFoldersPage/AddNodeButton.tsx
+++ src/ts/frontend/components/mainFoldersPage/AddNodeButton.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import { useDispatch, useSelector } from 'react-redux';
 import { Dispatch } from "redux";
 
@@ -13,6 +13,8 @@ const SELECTION_DIV_CLASS_NAME = 'add-folder-item-selection'
 const AddNodeButton = () => {
     const dispatch: Dispatch<FolderActionT | ModalActionT> = useDispatch();
     const { isAddItemModalOpen } = useSelector((state: RootState) => state.modalState);
+    const { userRole } = useSelector((state: RootState) => state.userState);
+    const isEditorRef = useRef(false)
     const [isAddNodeBtnClicked, setIsAddNodeBtnClicked] = useState(false);
     const [isSelectionMenuVisible, setIsSelectionMenuVisible] = useState(false);
 
@@ -47,12 +49,24 @@ const AddNodeButton = () => {
             dispatch(setIsAddItemModalOpen(true))
 
         } else if (btnType === "cancel") {
-            console.log('cancel selection')
+            console.log('cancel')
+
+        } else if (btnType === "exit") {
+            console.log('exit')
 
         } else throw new Error('Invalid type.')
 
         setIsSelectionMenuVisible(false);
     }
+
+    /* determines if user can edit or readonly */
+    if (!userRole) {
+        isEditorRef.current = false
+    }
+    else if (["editor", "publisher", "admin"].includes(userRole)) {
+        isEditorRef.current = true
+    }
+    else { isEditorRef.current = false }
 
     return (
         <>
@@ -67,7 +81,15 @@ const AddNodeButton = () => {
                     <path d="M64 80c-8.8 0-16 7.2-16 16V416c0 8.8 7.2 16 16 16H384c8.8 0 16-7.2 16-16V96c0-8.8-7.2-16-16-16H64zM0 96C0 60.7 28.7 32 64 32H384c35.3 0 64 28.7 64 64V416c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V96zM200 344V280H136c-13.3 0-24-10.7-24-24s10.7-24 24-24h64V168c0-13.3 10.7-24 24-24s24 10.7 24 24v64h64c13.3 0 24 10.7 24 24s-10.7 24-24 24H248v64c0 13.3-10.7 24-24 24s-24-10.7-24-24z" />
                 </svg>
             </div>
-            {isAddNodeBtnClicked &&
+            {/* read only mode */}
+            {!isEditorRef.current && isAddNodeBtnClicked &&
+                <div className={SELECTION_DIV_CLASS_NAME}>
+                    <span className="disabled-text">Feature disabled. <br /> READ-ONLY mode.</span>
+                    <button onClick={() => handleSelectionBtnClicks('exit')}>Exit</button>
+                </div>
+            }
+            {/* editor mode */}
+            {isEditorRef.current && isAddNodeBtnClicked &&
                 <div className={SELECTION_DIV_CLASS_NAME}>
                     <button onClick={() => handleSelectionBtnClicks('folder')}>Add Folder</button>
                     <button onClick={() => handleSelectionBtnClicks('item')}>Add Item</button>
