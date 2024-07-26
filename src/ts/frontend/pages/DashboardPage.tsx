1db83c7816c28718df836f068fcac652ed1713d7
fceja <88167774+fceja@users.noreply.github.com>
2024-07-26 16:58:08 -0700

    rename class

--- src/ts/frontend/pages/DashboardPage.tsx
+++ src/ts/frontend/pages/DashboardPage.tsx
@@ -1,7 +1,7 @@
 import "@scss/pages/DashboardPage.scss";
 
 const DashboardPage = () => {
-  return <div className="homepage-container">[TODO] - Dashboard Page</div>;
+  return <div className="dashboard-page">[TODO] - Dashboard Page</div>;
 };
 
 export default DashboardPage;
