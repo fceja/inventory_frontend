0be1e5ba9b74970ffa149889997286e3605f2774
fceja <88167774+fceja@users.noreply.github.com>
2024-07-30 19:11:32 -0700

    fix modal populating issue

--- src/ts/frontend/pages/MainFoldersPage.tsx
+++ src/ts/frontend/pages/MainFoldersPage.tsx
@@ -3,23 +3,26 @@ import { Dispatch } from "redux";
 import { useDispatch } from 'react-redux';
 
 import "@scss/pages/MainFoldersPage.scss"
-import { setFolderData, FolderActionT } from "@store/folder/FolderActions";
 import AddNodeButton from "@components/mainFoldersPage/AddNodeButton"
+import { setFolderData, FolderActionT } from "@store/folder/FolderActions";
 import FolderNavigation from "@components/mainFoldersPage/FolderNavigation"
 import FolderContentNodes from "@components/mainFoldersPage/FolderContentNodes"
 import FolderStats from "@components/mainFoldersPage/FolderStats"
+import { setSelectedItemId, ItemActionT } from "@store/item/ItemActions";
 
 const MainFoldersPage = () => {
-    const dispatch: Dispatch<FolderActionT> = useDispatch();
+    const dispatch: Dispatch<FolderActionT | ItemActionT> = useDispatch();
 
     useEffect(() => {
 
+        /* clear folder and item redux state */
         return () => {
             dispatch(setFolderData({
                 folderId: null,
                 folderName: null,
                 parentFolderId: null
             }))
+            dispatch(setSelectedItemId(null))
         }
     }, [])
 
