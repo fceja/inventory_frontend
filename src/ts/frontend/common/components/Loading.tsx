71e3d53aeedca2f591b038324ea6a8630bec1305
fceja <88167774+fceja@users.noreply.github.com>
2024-08-13 14:50:15 -0700

    add new loading comp

--- src/ts/frontend/common/components/Loading.tsx
+++ src/ts/frontend/common/components/Loading.tsx
@@ -1,5 +1,15 @@
-const Loading = () => {
-    return (<div>This is Loading.</div>)
+import "@scss/common/Loading.scss";
+
+interface LoadingI {
+    className?: string
+}
+const Loading = (props: LoadingI) => {
+    const { className } = props
+    return (
+        <div className={className ? `loading-${className}` : 'loading'}>
+            <div className="loading-spinner"></div>
+        </div>
+    )
 }
 
 export default Loading
